e32b8bd4312b8e274bbd3a584612cb8962d9e96d
Sergey Kuznetsov <kuzzz99@gmail.com>
2022-05-27 01:37:00 +0300

    Tmp commit. Setting up build

--- include/matrix.h
+++ include/matrix.h
@@ -1,4 +1,10 @@
 
 namespace linalg {
 
+template<size_t num_rows, size_t num_columns, typename T>
+class Matrix {};
+
+template<size_t num_rows, typename T>
+class Vector : public Matrix<num_rows, 1, T> {};
+
 }
